ff30212099bace8bdfe8e9a976a5a107c965fab4
phgiang1987 <phgiang1987@gmail.com>
2021-11-21 19:52:32 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  // @ViewChild('f')  signupForm: NgForm;
-  defaultQuestion: string = 'pet';
-  question: string = '';
   genders = ['male', 'female'];
+
+  signupForm: FormGroup = new FormGroup({
+    'username': new FormControl('', Validators.required),
+    'email': new FormControl('', [Validators.required, Validators.email]),
+    'gender': new FormControl('male'),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   resetName(){
     console.log('reset Name');
   }
 
-  onSubmit(form: NgForm): void {
-    console.log(form.value);
+  onSubmit() {
+    console.log(this.signupForm);
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
@@ -12,7 +12,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
