957ff1c81c5dcd105c3ed1101d5a30bb38086661
phgiang1987 <phgiang1987@gmail.com>
2021-12-24 22:01:49 +0700

    update

--- src/app/admin/blog/save/save.component.ts
+++ src/app/admin/blog/save/save.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormGroupDirective, FormGroup, FormControl, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-save-blog',
   templateUrl: './save.component.html',
   styleUrls: ['./save.component.scss']
 })
 export class SaveBlogComponent implements OnInit {
+  categories : Array<any> = [
+    { name: 'Apple', value: 'Apple' },
+    { name: 'Pear', value: 'Pear' },
+    { name: 'Orange', value: 'Orange' }
+  ]
+
+  formBlog : FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    excerpt: new FormControl('', [Validators.required]),
+    status: new FormControl('approve', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+    category: new FormArray([], [Validators.required]),
+    // slug: new FormControl('', [Validators.required]),
+  });
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onCheckboxChange(e: any) {
+    const checkArray: FormArray = this.formBlog.get('category') as FormArray;
+
+    console.log(e);
+
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: any) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  submitForm(form: FormGroupDirective){
+    console.log(this.formBlog.value);
+    console.log(form.value);
+  }
 }
--- src/app/admin/blog/save/save.module.ts
+++ src/app/admin/blog/save/save.module.ts
@@ -3,15 +3,32 @@ import { CommonModule } from '@angular/common';
 
 import { SaveBlogComponent } from './save.component';
 import { SaveBlogRoutingModule } from './save-routing.module';
-
-
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     SaveBlogComponent
   ],
   imports: [
     CommonModule,
-    SaveBlogRoutingModule
+    SaveBlogRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    EditorModule,
+    NzGridModule,
+    NzCardModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzSelectModule,
+    NzCheckboxModule
   ]
 })
 export class SaveBlogModule { }
