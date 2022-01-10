094e1b83d2f494c1b0769d4b16d2db4d37f08252
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2022-01-10 17:14:09 +0700

    Update category-name.pipe.ts

--- src/app/Pipe/category-name.pipe.ts
+++ src/app/Pipe/category-name.pipe.ts
@@ -6,8 +6,8 @@ import { BlogService } from '../Services/blog.service';
 })
 export class CategoryNamePipe implements PipeTransform {
   transform(id: any , categories: any[]) {
-      let category : any = categories.find((category: any) => category.id === id);
-      let result = category.name as string
+      let category : any = categories?.find((category: any) => category.id === id);
+      let result = category?.name as string
       console.log(typeof result)
       console.log(result)
       return result;
